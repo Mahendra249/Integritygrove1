9d81d161e368d403a87c392cd7b2093b6a3a855e
JAYESH-VIJAY <jayeshvijay649@gmail.com>
2024-01-22 00:55:31 +0530

    blog, blogdetails, portfolio completed

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -11,10 +11,53 @@ function ProjectPage() {
   return (
     <>
       <ProjectDesc />
-      <LeftProjects />
-      <RightProjects />
+      <section className="blog-list-half section-padding sub-bg">
+        <div className="container">
+          <div className="row">
+            <SingleProject/>
+            <SingleProject/>
+            <SingleProject/>
+            <SingleProject/>
+            <SingleProject/>
+            <SingleProject/>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
 
 export default ProjectPage;
+
+function SingleProject() {
+  return (
+    <div className="col-lg-6">
+      <div className="item mb-50">
+        <div className="row">
+          <div className="col-md-6 img">
+            <img
+              src="https://ui-themez.smartinnovates.net/items/geekfolio/dark/assets/imgs/blog/h1.jpg"
+              alt
+            />
+          </div>
+          <div className="col-md-6 main-bg cont valign">
+            <div className="full-width">
+              <span className="date fz-12 ls1 text-u opacity-7 mb-15">
+                August 6, 2022
+              </span>
+              <h5>
+                <a href="blog-details.html">
+                  Free advertising for your online business.
+                </a>
+              </h5>
+              <div className="tags colorbg mt-15">
+                <a href="blog-list.html">Marketing</a>
+                <a href="blog-list.html">Design</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
